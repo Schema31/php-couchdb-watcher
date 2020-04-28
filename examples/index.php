c58e212263ebffabe39e3a9013d835b84ba9b634
Antonio Turdo <aturdo@schema31.it>
2020-04-28 14:48:05 +0200

    Fix example

--- examples/index.php
+++ examples/index.php
@@ -5,8 +5,8 @@ require __DIR__."/../vendor/autoload.php";
 use Schema31\CouchDBWatcher\Watcher;
 
 $options = [
-  'since' => 'now',  // taken into account only if store is empty
-  'store' => new Schema31\CouchDBWatcher\Store\FileStore() // default store
+  'since' => 'now',  // taken into account only if store is empty, default is '0'
+  'store' => new Schema31\CouchDBWatcher\Store\FileStore() // FileStore is the default one. You can provide your own object, that implements the  StoreInterface
 ];
 
 $watcher = new Watcher('main_db', 'https://couch.com/db', $options);
